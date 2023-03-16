6c7e1b2823d191a2e6196377b61e84b1bb11c4de
rqphy <raphael.ferreira@hetic.net>
2023-03-17 00:39:13 +0100

    :art: tweak experience values

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,5 +1,4 @@
 import { Canvas } from "@react-three/fiber"
-import { OrbitControls } from "@react-three/drei"
 import Planet from "../planet/planet"
 import Satellite from "../satellite/satellite"
 
@@ -14,21 +13,9 @@ export default function Experience() {
 				position: [0, 0, 3],
 			}}
 		>
-			<OrbitControls
-				enableDamping={true}
-				enablePan={false}
-				maxPolarAngle={Math.PI / 2.06}
-				minPolarAngle={0}
-				dampingFactor={0.05}
-				rotateSpeed={0.3}
-				maxDistance={8.0}
-				minDistance={3.8}
-			/>
 			<directionalLight
-				castShadow
-				position={[0, 0, 4]}
+				position={[3, 2, 4]}
 				intensity={1.5}
-				shadow-normalBias={0.09}
 				color={"#e8dfdd"}
 			/>
 			<ambientLight intensity={0.2} />
