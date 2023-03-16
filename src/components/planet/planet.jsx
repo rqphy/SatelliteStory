fe10d6496a70812285e34be749a907fcf4c9f0a2
rqphy <raphael.ferreira@hetic.net>
2023-03-17 00:17:55 +0100

    :bento: planet > update model

--- src/components/planet/planet.jsx
+++ src/components/planet/planet.jsx
@@ -1,14 +1,16 @@
 import { useGLTF } from "@react-three/drei"
 
 export default function Planet() {
-	const planet = useGLTF("./low_poly_planet_earth/scene.gltf")
+	const planet = useGLTF(
+		"./planet_earth_alt-drag_to_change_lighting/scene.gltf"
+	)
 
 	return (
 		<primitive
 			object={planet.scene}
 			scale={2.5}
-			rotation={[0, Math.PI * 2.2, 0]}
-			position={[0, -6, 0]}
+			rotation={[0, 0, Math.PI * 0.5]}
+			position={[0, -2.5, 0]}
 		/>
 	)
 }
