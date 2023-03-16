19663ec33530ffb1c74956e3c469c45001f1fc51
rqphy <raphael.ferreira@hetic.net>
2023-03-17 00:37:09 +0100

    :construction: add title

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -3,6 +3,11 @@ import Experience from "../../components/experience/experience"
 export default function Home() {
 	return (
 		<div>
+			<h1>
+				<span>Welcome to,</span>
+				<br />
+				Satellite Story
+			</h1>
 			<Experience />
 		</div>
 	)
