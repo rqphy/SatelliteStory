4c9859fc8b97f365a6f6f0d350fc2517fd038987
rqphy <raphael.ferreira@hetic.net>
2023-03-19 11:13:29 +0100

    :sparkles: home > use content component to display text

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,13 +1,17 @@
 import Experience from "../../components/experience/experience"
+import Content from "../../components/content/content"
+import data from "../../data/content.json"
 
 export default function Home() {
 	return (
 		<div>
-			<h1>
-				<span>Welcome to,</span>
-				<br />
-				Satellite Story
-			</h1>
+			<Content>
+				<h1>
+					<span>Welcome to,</span>
+					<br />
+					{data.title}
+				</h1>
+			</Content>
 			<Experience />
 		</div>
 	)
