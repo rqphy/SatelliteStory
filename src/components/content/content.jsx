982f3a0379abaee5efd625977149f860dca637df
rqphy <raphael.ferreira@hetic.net>
2023-03-19 11:53:03 +0100

    :construction: apply default animation

--- src/components/content/content.jsx
+++ src/components/content/content.jsx
@@ -1,3 +1,27 @@
+import Animated from "react-mount-animation"
+import { useState } from "react"
+
+const mountAnimation = ` 
+    0% {opacity: 0}
+    100% {opacity: 1}
+`
+
+const unmoutAnimation = ` 
+    0% {opacity: 1}
+    100% {opacity: 0}
+`
+
 export default function Content({ children }) {
-	return <div className="content">{children}</div>
+	const [isMounted, setIsMounted] = useState(false)
+	return (
+		<Animated.div
+			className="content"
+			show={isMounted}
+			mountAnim={mountAnimation}
+			unmountAnim={unmoutAnimation}
+			time={0.6}
+		>
+			{children}
+		</Animated.div>
+	)
 }
