d231196a34edf4e4fc6745e572c7afc6cf78af28
rqphy <raphael.ferreira@hetic.net>
2023-03-25 10:59:27 +0100

    :construction: custom loader > add default content

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -1,11 +1,16 @@
+import "./loader.scss"
 import { useProgress } from "@react-three/drei"
 
 export default function Loader({ hasStarted, onStart }) {
 	const { progress } = useProgress()
-	console.log(progress)
 
 	return (
 		<section className={`loader ${hasStarted ? "loader_started" : ""}`}>
+			<div className="loader__title">
+				<p>Welcome to,</p>
+				<h1>A Satellite Story</h1>
+			</div>
+			<div className="loader__orbit"></div>
 			<button disabled={progress < 100} onClick={onStart}>
 				Enter the experience {progress}
 			</button>
