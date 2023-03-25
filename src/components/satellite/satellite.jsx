e5fe0ae91d39d0b0f8e3fdccb56946827c290398
rqphy <raphael.ferreira@hetic.net>
2023-03-25 14:54:29 +0100

    :dizzy: satellite > add animation

--- src/components/satellite/satellite.jsx
+++ src/components/satellite/satellite.jsx
@@ -1,10 +1,19 @@
 import { useGLTF } from "@react-three/drei"
+import { useFrame } from "@react-three/fiber"
+import { useRef } from "react"
 
 export default function Satellite() {
 	const satellite = useGLTF("./sputnik/scene.gltf")
+	const satelliteRef = useRef()
+
+	useFrame((state, delta) => {
+		satelliteRef.current.rotation.x += 0.2 * delta
+		satelliteRef.current.rotation.z += 0.1 * delta
+	})
 
 	return (
 		<primitive
+			ref={satelliteRef}
 			object={satellite.scene}
 			scale={0.3}
 			rotation={[Math.PI * 0, Math.PI * 0, Math.PI * 0]}
