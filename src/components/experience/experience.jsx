bdbe3f16419a764c04f9ef30db5562635987a0d4
rqphy <raphael.ferreira@hetic.net>
2023-04-08 17:39:05 +0200

    :sparkles: add stars with drei, remove own made stars compoenent

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,29 +1,38 @@
 import { Canvas } from "@react-three/fiber"
-import { ScrollControls } from "@react-three/drei"
+import { ScrollControls, Stars } from "@react-three/drei"
 import Planet from "../planet/planet"
 import Satellite from "../satellite/satellite"
 
 export default function Experience() {
-	return (
-		<Canvas
-			shadows
-			camera={{
-				fov: 45,
-				near: 0.1,
-				far: 200,
-				position: [0, 0, 3],
-			}}
-		>
-			<directionalLight
-				position={[3, 2, 4]}
-				intensity={1}
-				color={"#e8dfdd"}
-			/>
-			<ambientLight intensity={0.2} />
-			<ScrollControls distance={2} pages={6} damping={0.2}>
-				<Planet />
-			</ScrollControls>
-			<Satellite />
-		</Canvas>
-	)
+    return (
+        <Canvas
+            shadows
+            camera={{
+                fov: 45,
+                near: 0.1,
+                far: 200,
+                position: [0, 0, 3],
+            }}
+        >
+            <directionalLight
+                position={[3, 2, 4]}
+                intensity={1}
+                color={"#e8dfdd"}
+            />
+            <ambientLight intensity={0.2} />
+            <ScrollControls distance={2} pages={6} damping={0.2}>
+                <Planet />
+            </ScrollControls>
+            <Satellite />
+            <Stars
+                radius={20}
+                depth={50}
+                count={3000}
+                factor={3}
+                saturation={0}
+                fade
+                speed={1}
+            />
+        </Canvas>
+    )
 }
