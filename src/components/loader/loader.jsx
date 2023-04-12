1b0452f9187161dc0a978c90a9efe84e4eaedb5c
rqphy <raphael.ferreira@hetic.net>
2023-04-12 11:59:03 +0200

    :bug::iphone: fix 100vh scroll issue on mobile

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -1,30 +1,29 @@
-import "./loader.scss"
 import { useProgress } from "@react-three/drei"
 
 export default function Loader({ hasStarted, onStart }) {
-	const { progress } = useProgress()
+    const { progress } = useProgress()
 
-	return (
-		<section className={`loader ${hasStarted ? "loader_started" : ""}`}>
-			<div className="loader__title">
-				<p>Welcome to,</p>
-				<h1>A Satellite Story</h1>
-			</div>
-			<figure className="loader__orbit">
-				<img src="/earth.png" alt="earth" />
-				<div className="loader__satellite"></div>
-			</figure>
-			<button
-				className="loader__cta"
-				disabled={progress < 100}
-				onClick={onStart}
-			>
-				<p>{progress < 100 ? "Loading..." : "Enter the experience"}</p>
-				<div
-					className="loader__progress"
-					style={{ left: `${progress - 100}%` }}
-				></div>
-			</button>
-		</section>
-	)
+    return (
+        <section className={`loader ${hasStarted ? "loader_started" : ""}`}>
+            <div className="loader__title">
+                <p>Welcome to,</p>
+                <h1>A Satellite Story</h1>
+            </div>
+            <figure className="loader__orbit">
+                <img src="/earth.png" alt="earth" />
+                <div className="loader__satellite"></div>
+            </figure>
+            <button
+                className="loader__cta"
+                disabled={progress < 100}
+                onClick={onStart}
+            >
+                <p>{progress < 100 ? "Loading..." : "Enter the experience"}</p>
+                <div
+                    className="loader__progress"
+                    style={{ left: `${progress - 100}%` }}
+                ></div>
+            </button>
+        </section>
+    )
 }
